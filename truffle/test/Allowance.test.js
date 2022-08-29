d894164928120e5f6562f1757f1b0a21b8afde33
Sayem98 <sayemabedin.bd@gmail.com>
2022-08-29 14:19:22 +0600

    Testing contract

--- truffle/test/Allowance.test.js
+++ truffle/test/Allowance.test.js
@@ -1,1 +1,21 @@
+const Allowance = artifacts.require("Allowance");
 
+// require("chai").use(require("chai-as-promised")).should();
+
+contract("Allowance", () => {
+  let instance;
+  before(async () => {
+    // instance = await Allowance.deployed();
+    instance = await Allowance.new();
+  });
+  it("Should get the deployed contract address", async () => {
+    console.log(instance.address);
+    assert(instance.address != null, "Not instanciated");
+  });
+
+  it("Should get balance of the contract", async () => {
+    // const instance = await Allowance.deployed();
+    const balance = await instance.Balance();
+    assert.equal(balance, 0, "Balance is not Empty.");
+  });
+});
